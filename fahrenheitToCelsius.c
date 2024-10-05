795e52ecc2dcbf6186bde91f8a8c280e291921b1
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-10-05 16:26:36 +0000

    added new file

---
+++ fahrenheitToCelsius.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+/* print Celsius-Fahrenheit table
+ for celsius = -100, -95, -90 ..., 100 */
+
+{
+    int fahr, celsius;
+    int lower, upper, step;
+    lower = -100; /* lower limit of temperature scale */
+    upper = 100; /* upper limit */
+    step = 5; /* step size */
+
+    celsius = lower;
+
+    while (celsius <= upper)
+    {
+        fahr = (5 * celsius / 9 - 32);
+        printf("%d\t%d\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
