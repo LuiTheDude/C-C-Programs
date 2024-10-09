fb6feb6701440919a7318f1e467e50d65302bad9
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-10-09 08:10:03 +0000

    added a file

---
+++ charCount.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* count characters in input; 2nd version */
+int main() {
+    int nc;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%d\n", nc);
+    return 0;
+}
+
+// ctrl+D signals EOF
