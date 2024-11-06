e7207aa701174d08ff081dfdd4172efafec6976c
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-11-06 08:04:50 +0000

    did 2.8 rightrot

--- chapter2/rightrot.c
+++ chapter2/rightrot.c
@@ -1,2 +1,35 @@
 #include <stdio.h>
 
+unsigned rightrot(unsigned x, int n){
+    int numBits = sizeof(x) * 8;
+
+    n %= numBits;
+
+    unsigned rightPart = x >> n;
+    unsigned leftPart = x << (numBits - n);
+
+    return rightPart | leftPart;
+}
+
+void printBinary(unsigned x){
+    for (int i = 31; i >= 0; i--){
+        unsigned bit = (x >> i) & 1;
+        printf("%u", bit);
+    }
+    printf("\n");
+}
+
+int main(){
+    unsigned x = 0b0011010111001;
+    int n = 4;
+
+    unsigned result = rightrot(x, n);
+
+    printf("Initial decimal: %u\n", x);
+    printf("Binary: ");
+    printBinary(x);
+
+    printf("Decimal: %u\n", result);
+    printf("Binary: ");
+    printBinary(result);
+}
