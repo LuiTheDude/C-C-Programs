820fede97d0983cba6a8156d23907845046206ef
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-11-07 13:10:35 +0000

    did 2.10

---
+++ chapter2/lowerWithTernary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* lower:  convert c to lower case; ASCII only */
+int lower(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
+
+int main(){
+    printf("%c\n", lower('A'));
+}
