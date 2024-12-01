c6e4f528e50a40d20cfd0d8bbe765856343ec9d0
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-12-01 19:15:19 +0400

    made changes to assembly

--- assemblyExperiments/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-//test comment
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
---
+++ assemblyExperiments/while.s
@@ -0,0 +1,55 @@
+	.file	"while.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"ayo"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1337, -4(%rbp)
+	jmp	.L2
+.L3:
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+.L2:
+	movl	-4(%rbp), %eax
+	leal	-1(%rax), %edx
+	movl	%edx, -4(%rbp)
+	testl	%eax, %eax
+	jne	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.2.0-23ubuntu4) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
